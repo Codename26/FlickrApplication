5a41baaa1293bba6397a299b5bb693c18880d8c0
Codename26 <slava.burchenko@gmail.com>
2017-12-26 00:47:55 +0200

    Minor fixes

--- app/src/main/java/com/example/user/flickrapplication/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/user/flickrapplication/PhotoGalleryFragment.java
@@ -1,18 +1,25 @@
 package com.example.user.flickrapplication;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
 public class PhotoGalleryFragment extends Fragment {
 
+    private static final String TAG = "PhotoGalleryFragment";
     private RecyclerView mPhotoRecyclerView;
 
     public static PhotoGalleryFragment newInstance(){
@@ -28,6 +35,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemTask().execute();
     }
 
     @Override
@@ -38,6 +46,22 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return v;
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                String result = new FlickrFetchr()
+                        .getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "fetched contents of URL: " + result);
+
+            } catch (IOException ioe){
+                Log.e(TAG, "failed to fetch URL: ", ioe );
+            }
+            return null;
+        }
     }
 
 }
